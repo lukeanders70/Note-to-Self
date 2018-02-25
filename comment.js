f6d3f8f94d63da52f530c6c2e2d5faedf663fbd2
lukeanders70 <luke.larson@berkeley.edu>
2018-02-25 14:57:43 -0800

    comments now save and display correctly on corresponding urls

--- comment.js
+++ comment.js
@@ -8,41 +8,49 @@ function getUrl(callback) {
   });
 }
 
-function addComment(){
-	console.log("addComment start")
-	getUrl((url)=>{
-	  var new_comment = document.getElementById('comment_text').value;
-	  getCurrentComments(url, (current_comments) => {
-		  if (current_comments === undefined || current_comments.length == 0){
-		  	current_comments = [new_comment];
-		  }
-		  else{
-		  	current_comments.push(new_comment);
-		  }
-		  var items = {};
-		  items[url] = current_comments;
-		  chrome.storage.sync.set(items)
-		  document.getElementById("new_comment").textContent= new_comment;
-		});
-	});  
-}
-
 function getCurrentComments(url, callback){
 	chrome.storage.sync.get(url, (comments) => {
 		callback(chrome.runtime.lastError ? null : comments[url])
 	});
 }
 
-/**
-function getSavedBackgroundColor(url, callback) {
-  chrome.storage.sync.get(url, (items) => {
-    callback(chrome.runtime.lastError ? null : items[url]);
-  });
+function loadCurrentComments(url){
+	getCurrentComments(url, (current_comments) => {
+		var comment;
+		console.log(current_comments)
+		for (i = 0; i < current_comments.length; i++){
+			document.getElementById("current_comments").innerHTML = document.getElementById("current_comments").innerHTML + "<br>" + current_comments[i];
+		}
+	});
 }
 
-function saveBackgroundColor(url, color) {
-  var items = {};
-  items[url] = color;
+document.addEventListener('DOMContentLoaded', () => {
+	getUrl((url)=>{
+		document.getElementById("comment_button").addEventListener("click", function(){
+		  getCurrentComments(url, (current_comments) => {
+		  	console.log(current_comments)
+			  var new_comment = document.getElementById('comment_text').value;
 
-  chrome.storage.sync.set(items);
-}**/
+			  if (current_comments === undefined || current_comments.length == 0){
+			  	current_comments = [new_comment];
+			  }
+			  else{
+			  	current_comments.push(new_comment);
+			  }
+
+			  //store comments for this URL as list in local memory
+			  var comments_for_storage = {};
+			  comments_for_storage[url] = current_comments;
+			  chrome.storage.sync.set(comments_for_storage)
+
+			  //add the newest comment to the display and remove text from input box (not done automatically because this is not a submit button)
+			  document.getElementById("current_comments").innerHTML = document.getElementById("current_comments").innerHTML + "<br>" + new_comment;
+			  document.getElementById('comment_text').value = "";
+			});
+		});
+
+		loadCurrentComments(url);
+
+	});
+});
+
