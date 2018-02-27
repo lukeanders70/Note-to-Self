0db5068cd5f733efa9dbf265a890e57584234f6d
lukeanders70 <luke.larson@berkeley.edu>
2018-02-27 13:41:13 -0800

    fixed bug where you couldn't delete comments in same session as adding a new onw

--- comment.js
+++ comment.js
@@ -1,5 +1,9 @@
 //The HTML needed to inject for each comment. By splitting it up into start, middle end
 //it is possible to dynamically construct the unique html code each comment requires
+
+
+//problem: when I replace the innerHTML, I'm changing the element and, in so doing, removing my eventlisteners
+
 var comment_block_html_1 = "<div class='comment_block' id=";
 var comment_block_html_2 = "'><span class='comment_text'>";
 var comment_block_html_3 = "</span><button class ='delete_button' title='delete comment' id=";
@@ -24,21 +28,33 @@ function getCurrentComments(url, callback){
 function loadCurrentComments(url){
   getCurrentComments(url, (current_comments) => {
     var comment;
-    console.log(current_comments)
     for (i = 0; i < current_comments.length; i++){
-      document.getElementById("current_comments").innerHTML = document.getElementById("current_comments").innerHTML +
-                                                              comment_block_html_1 +
-                                                              "'comment_block_" + i + "'" +
-                                                              comment_block_html_2 +
-                                                              current_comments[i] +
-                                                              comment_block_html_3 +
-                                                              "'delete_comment_" + i + "'" +
-                                                              comment_block_html_4;
+      new_div = document.createElement('div');
+      new_div.setAttribute("id", "comment_block_" + i);
+      new_div.setAttribute("class", "comment_block");
+
+      new_span = document.createElement('span');
+      new_span.setAttribute("class", "comment_text");
+
+      new_button = document.createElement('button');
+      new_button.setAttribute("id", "delete_comment_" + i);
+      new_button.setAttribute("class", "delete_button");
+      new_button.setAttribute("title", "delete comment");
+
+      new_text = document.createTextNode(current_comments[i]);
+      x_text = document.createTextNode("x")
+
+      new_span.appendChild(new_text);
+      new_button.appendChild(x_text)
+      new_div.appendChild(new_span);
+      new_div.appendChild(new_button);
+
+      document.getElementById("current_comments").appendChild(new_div);
     }
   });
 }
 
-function deleteCommentFunction(i, current_comments, url){
+function deleteCommentFunction(i, current_comments, url, callback){
   var delete_i = function(){
     document.getElementById("comment_block_" + i.toString()).remove();
     current_comments.splice(i, 1);
@@ -46,7 +62,7 @@ function deleteCommentFunction(i, current_comments, url){
     comments_for_storage[url] = current_comments;
     chrome.storage.sync.set(comments_for_storage);
   }
-  return delete_i
+  callback(delete_i)
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -70,21 +86,45 @@ document.addEventListener('DOMContentLoaded', () => {
         comments_for_storage[url] = current_comments;
         chrome.storage.sync.set(comments_for_storage);
 
+        //this new comment will be the jth comment
+        j = current_comments.length - 1;
+
         //add the newest comment to the display and remove text from input box (not done automatically because this is not a submit button)
-        document.getElementById("current_comments").innerHTML = document.getElementById("current_comments").innerHTML +
-                                                              comment_block_html_1 +
-                                                              "'comment_block_" + i + "'" +
-                                                              comment_block_html_2 +
-                                                              new_comment +
-                                                              comment_block_html_3 +
-                                                              "'delete_comment_" + i + "'" +
-                                                              comment_block_html_4;
-        document.getElementById('comment_text').value = "";
+
+        new_div = document.createElement('div');
+        new_div.setAttribute("id", "comment_block_" + j);
+        new_div.setAttribute("class", "comment_block");
+
+        new_span = document.createElement('span');
+        new_span.setAttribute("class", "comment_text");
+
+        new_button = document.createElement('button');
+        new_button.setAttribute("id", "delete_comment_" + j);
+        new_button.setAttribute("class", "delete_button");
+        new_button.setAttribute("title", "delete comment");
+
+        new_text = document.createTextNode(new_comment);
+        x_text = document.createTextNode("x")
+
+        new_span.appendChild(new_text);
+        new_button.appendChild(x_text)
+        new_div.appendChild(new_span);
+        new_div.appendChild(new_button);
+
+        document.getElementById("current_comments").appendChild(new_div);
+
+        deleteCommentFunction(j, current_comments, url, (deleteFunction) => {
+          document.getElementById("delete_comment_" + j.toString()).addEventListener("click", deleteFunction);
+          document.getElementById('comment_text').value = "";
+        });
       });
 
       for (i = 0; i < current_comments.length; i++){
         //setting listeners for delete buttons
-        document.getElementById("delete_comment_" + i.toString()).addEventListener("click", deleteCommentFunction(i, current_comments, url));
+        deleteCommentFunction(i, current_comments, url, (deleteFunction) => {
+          document.getElementById("delete_comment_" + i.toString()).addEventListener("click", deleteFunction);
+          document.getElementById('comment_text').value = "";
+        });
 
       }
     });
