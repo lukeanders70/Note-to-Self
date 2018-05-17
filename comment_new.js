1c894b69f4d877baff809987538c1115ca54be0f
lukeanders70 <luke.larson@berkeley.edu>
2018-05-16 23:36:51 -0700

    fixed flashing

--- comment_new.js
+++ comment_new.js
@@ -43,14 +43,15 @@ var get_current_comments = function(callback){
 
 var load_comment = function(comment, i, comment_type){
     var new_div = document.createElement('div');
-    new_div.setAttribute("id", "comment_block_" + i);
+    new_div.setAttribute("id", "comment_block_" + comment_type + "_" + i);
     new_div.setAttribute("class", "comment_block");
 
     var new_span = document.createElement('span');
+    new_span.setAttribute("id", "comment_text_" + comment_type + "_" + i);
     new_span.setAttribute("class", "comment_text");
 
     var new_button = document.createElement('button');
-    new_button.setAttribute("id", "delete_comment_" + i);
+    new_button.setAttribute("id", "delete_comment_" + comment_type + "_" + i);
     new_button.setAttribute("class", "delete_button");
     new_button.setAttribute("title", "delete comment");
 
@@ -67,24 +68,35 @@ var load_comment = function(comment, i, comment_type){
 
 var load_current_comments = function(){
 
-    document.getElementById("current_comments_domain").innerHTML = "";
-    document.getElementById("current_comments_url").innerHTML = "";
-
     get_current_comments(function(){
+
         for (var i = 0; i < current_comments["domain"].length; i++){
-            load_comment(current_comments["domain"][i], i, "domain");
-            delete_function = delete_comment_function(i)
-            document.getElementById("delete_comment_" + i.toString()).addEventListener("click", delete_function);
+            if(document.getElementById("comment_text_domain_" + i.toString())){
+                document.getElementById("comment_text_domain_" + i.toString()).innerHTML = current_comments["domain"][i]
+            }
+            else{
+                load_comment(current_comments["domain"][i], i, "domain");
+                delete_function = delete_comment_function(i)
+                document.getElementById("delete_comment_domain_" + i.toString()).addEventListener("click", delete_function);
+            }
+        }
+        if(document.getElementById("comment_block_domain_" + (current_comments["domain"].length)) ){
+            document.getElementById("comment_block_domain_" + (current_comments["domain"].length)).remove();
         }
 
         for (var i = 0; i < current_comments["url"].length; i++){
-            console.log(i)
-            load_comment(current_comments["url"][i], i + current_comments["domain"].length, "url");
-            delete_function = delete_comment_function(i);
-            document.getElementById("delete_comment_" + (i + current_comments["domain"].length).toString()).addEventListener("click", delete_function);
-
+            if(document.getElementById("comment_text_url_" + i.toString())){
+                document.getElementById("comment_text_url_" + i.toString()).innerHTML = current_comments["url"][i]
+            }
+            else{
+                load_comment(current_comments["url"][i], i, "url");
+                delete_function = delete_comment_function(i)
+                document.getElementById("delete_comment_url_" + i.toString()).addEventListener("click", delete_function);
+            }
         }
-    });
+        if(document.getElementById("comment_block_url_" + (current_comments["url"].length).toString())){
+            document.getElementById("comment_block_url_" + (current_comments["url"].length).toString()).remove();
+        }    });
 
 }
 
